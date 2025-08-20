200381d979369d3f21d3e62a68104f02f8a7e590
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-08-20 14:04:43 +0000

    Updated the author model

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5adbf4db6b482c43"; }
+/******/ 		__webpack_require__.h = function() { return "19b52dfda3dd32b4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
