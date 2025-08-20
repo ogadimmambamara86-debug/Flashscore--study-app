c8f12182064d13c9cf207268b65a585fee87cb73
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-08-20 13:38:56 +0000

    Updated the .gitignore and readme.md

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "63a2419fd2c6ab63"; }
+/******/ 		__webpack_require__.h = function() { return "5adbf4db6b482c43"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
