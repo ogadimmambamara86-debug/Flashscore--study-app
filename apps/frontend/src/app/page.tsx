e475090adcc4b05a34447fa6d4cb47e1cf7bfb3f
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-09-18 16:14:36 +0100

    Update page.tsx

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React from "react";
-import BackgroundParticles from "./.../../components/BackgroundParticles";
-import Header from "./.../../components/Header";
-import MissionBriefing from "./.../../components/MissionBriefing";
-import ModulesGrid from "./.../../components/ModulesGrid";
-import { useOfflineStatus } from "./hooks/useOfflineStatus";
-import OfflineManager from "./.../../components/OfflineManager";
+import BackgroundParticles from "../components/BackgroundParticles";
+import Header from "../components/Header";
+import MissionBriefing from "../components/MissionBriefing";
+import ModulesGrid from "../components/ModulesGrid";
+import { useOfflineStatus } from "../hooks/useOfflineStatus";
+import OfflineManager from "../components/OfflineManager";
 
 export default function Home() {
   const isOffline = useOfflineStatus();
