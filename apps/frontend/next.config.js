0d171d39c00d9155b6f05df0c562a4554dde40eb
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-09-18 21:21:17 +0100

    Update next.config.js

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,21 +1,15 @@
 // apps/frontend/next.config.js
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@components': path.resolve(__dirname, 'src/app/components'),
-      '@hooks': path.resolve(__dirname, 'src/app/hooks'),
-      '@services': path.resolve(__dirname, 'src/app/services'),
-      '@controllers': path.resolve(__dirname, 'src/app/controllers'),
-      '@api': path.resolve(__dirname, 'src/app/api'),
-      '@styles': path.resolve(__dirname, 'src/app/styles'), // Add this line
-    };
-    return config;
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*', // Proxy API requests to Express
+      },
+    ];
   },
 }
 
