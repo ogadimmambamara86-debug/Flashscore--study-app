6945fe77dea72485170657082a0e44a1f536e516
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-09-19 13:24:24 +0100

    Update next.config.js

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,8 +1,15 @@
 // apps/frontend/next.config.js
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
   async rewrites() {
     return [
       {
@@ -11,6 +18,18 @@ const nextConfig = {
       },
     ];
   },
-}
 
-export default nextConfig
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@components': path.resolve(__dirname, 'src/app/components'),
+      '@hooks': path.resolve(__dirname, 'src/app/hooks'),
+      '@controllers': path.resolve(__dirname, 'src/app/controllers'),
+      '@api': path.resolve(__dirname, 'src/app/api'),
+      '@shared': path.resolve(__dirname, '../../packages/shared/src'),
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
