3b7aeea0adbfc0e867cb77a07da879f5cde47bd2
MAGAJIco <97481521+MAGAJIco@users.noreply.github.com>
2025-09-24 10:19:19 +0100

    Update scraper.ts

--- apps/backend/src/routes/scraper.ts
+++ apps/backend/src/routes/scraper.ts
@@ -1,9 +1,21 @@
 // apps/backend/src/routes/scraper.ts
 import { FastifyInstance } from "fastify";
-import { saveScrapedMatches } from "../services/scraperService";
+import {
+  scrapeStakeOdds,
+  scrapeBetTodayPredictions,
+  saveScrapedMatches,
+} from "../services/scraperService";
 
 export async function scraperRoutes(server: FastifyInstance) {
-  server.get("/scrape", async () => {
+  server.get("/scrape/odds", async () => {
+    return await scrapeStakeOdds();
+  });
+
+  server.get("/scrape/predictions", async () => {
+    return await scrapeBetTodayPredictions();
+  });
+
+  server.get("/scrape/save", async () => {
     await saveScrapedMatches();
     return { message: "Scraping completed and saved to DB." };
   });
